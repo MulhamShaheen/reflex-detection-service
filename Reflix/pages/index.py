1d14affaf72fe081c680232d43c72c275397fdbc
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-12-16 14:04:39 +0300

    Add landing page

--- Reflix/pages/index.py
+++ Reflix/pages/index.py
@@ -1,6 +1,5 @@
 """The home page of the app."""
 
-from Reflix import styles
 from Reflix.templates import template
 
 import reflex as rx
@@ -13,13 +12,39 @@ def index() -> rx.Component:
     Returns:
         The UI for the home page.
     """
-    rx.heading("Main", font_size="3em"),
-    rx.text("Welcome to Reflex!"),
-    rx.text(
-        "You can edit this page in ",
-        rx.code("{your_app}/pages/settings.py"),
-    ),
-    with open("README.md", encoding="utf-8") as readme:
-        content = readme.read()
 
-    return rx.markdown(content, component_map=styles.markdown_style)
+    return rx.vstack(
+        rx.heading("Reflex People Detection Service", font_size="2em"),
+        rx.text("A service created to demonstrate web framework ",
+                rx.link("Reflex", href="https://reflex.dev/")),
+        rx.divider(),
+        rx.grid(
+            rx.grid_item(
+                rx.card(
+                    rx.text(
+                        "Image upload form page, where you can upload an image for processing\n "
+                        "For detection, the service uses ",
+                        rx.code("YOLOv8")),
+                    header=rx.heading("Main", size="lg"),
+                    footer=rx.link("To main >", href="/main", size="sm"),
+                    size="md",
+                    justify="space-between"
+                ),
+                row_span=1,
+                col_span=1
+            ),
+            rx.grid_item(
+                rx.card(
+                    rx.text("The page where you can see the history of you user requests. "
+                            "Authentication not available yet"),
+                    header=rx.heading("Dashboard", size="lg"),
+                    footer=rx.link("To dashboard >", href="/dashboard", size="sm"),
+                    size="md",
+                    justify="space-between"
+                ),
+                row_span=1, col_span=1),
+            template_columns="repeat(2, 1fr)",
+            h="100%",
+            gap=4,
+        )
+    )
